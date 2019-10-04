9ee6dc4ffbf65b7850afa6265b25b6b827fcac52
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-04 07:30:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -105,7 +105,10 @@ end
 def num_points_scored(player_s)
   game_hash.each do |team,team_info|
     team_info.each[:players].each do |player|
-      if player[:]      
+      if player[:player_name] == player_s
+      
+      
+      end
 
   end
 end
