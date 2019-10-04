85f62ea27e135875780042a0e182bded4ef7aee1
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-04 07:48:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -141,21 +141,20 @@ end
 
 
 
-def player_number(input)
-  output=[]
-  game_hash.each do |team,team_info|
-    if team_info[:team_name] == input
-      team_info.each do |key ,value|
-      if  key == :players
+def player_numbers(input)
+  output = []
+  game_hash.each do |team, team_info|
+    if team_info[:team_name] == input 
+      team_info.each do |key, value|
+        if key == :players
           value.each do |player|
           output.push(player[:number])
           end
+        end
       end
     end
   end
-  
-end
-return output
+  return output
 end
 
   
