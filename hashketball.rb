9a732364efb58f78f09992222f40985359f9da06
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-04 07:12:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,9 +3,10 @@ def game_hash
   {
     home:{ team_name:"Brooklyn Nets",
            colors:["Black","White"]
-           players:
-          
-      
+           players:{ player_name:"Alan Anderson"
+           number:
+             
+           }
     },
     away:{
     
