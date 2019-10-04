db013652276855b3e838a4e599393a239a327746
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-04 07:22:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -66,6 +66,47 @@ def game_hash
               steals:2,
               blocks:7,
               slam_dunks:2
+            },{
+              player_name:"Bismack Biyombo"
+              number:0,
+              shoe:16,
+              points:12,
+              rebounds:4,
+              assist:7,
+              steals:22,
+              blocks:15,
+              slam_dunks:10
+            },{
+              player_name:"DeSagna Diop"
+              number:2,
+              shoe:14,
+              points:24,
+              rebounds:12,
+              assist:12,
+              steals:4,
+              blocks:5,
+              slam_dunks:5
+            },{
+              player_name:"Ben Gordon"
+              number:8,
+              shoe:15,
+              points:33,
+              rebounds:3,
+              assist:2,
+              steals:1,
+              blocks:1,
+              slam_dunks:0
+            },
+            {
+              player_name:"Jeff Adrien"
+              number:4,
+              shoe:18,
+              points:10,
+              rebounds:1,
+              assist:1,
+              steals:2,
+              blocks:7,
+              slam_dunks:2
             }
               
             ]
